70a743ba329c2791481e09c6e3df5aa15bc5e00c
michfish966 <michfish966@ed.amdsb.ca>
2026-01-13 10:23:45 -0500

    Mike

--- ISC3C Final Coding Project/Form1.cs
+++ ISC3C Final Coding Project/Form1.cs
@@ -6,5 +6,16 @@ namespace ISC3C_Final_Coding_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            //g.FillRectangle(blackBrush, ball);
+        }
     }
 }
